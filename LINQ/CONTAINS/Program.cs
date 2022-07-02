a23fa056869f7d2d70865464a9591db1bfb4420a
RobertoRueda <roberto.daniel.rueda@gmail.com>
2022-07-02 20:00:46 -0300

    Add Linq Contains

---
+++ LINQ/CONTAINS/Program.cs
@@ -0,0 +1,67 @@
+﻿
+using ANY;
+using CONTAINS;
+
+
+//Contains checks if given element is present in the collection
+
+var numeros = Enumerable.Range(1, 10);
+
+var estaElNumero3 = numeros.Contains(3); //true
+Console.WriteLine($"estaElNumero3: {estaElNumero3}");
+
+var estaElNumero20 = numeros.Contains(20); //false
+Console.WriteLine($"estaElNumero20: {estaElNumero20}");
+
+var personas = new List<Persona>() {
+    new Persona { Nombre = "Eduardo", Edad = 30, FechaIngresoALaEmpresa = new DateTime(2021, 1, 2), Soltero = true },
+    new Persona { Nombre = "Nidia", Edad = 19, FechaIngresoALaEmpresa = new DateTime(2015, 11, 22), Soltero = true },
+    new Persona { Nombre = "Alejandro", Edad = 45, FechaIngresoALaEmpresa = new DateTime(2020, 4, 12), Soltero = false },
+    new Persona { Nombre = "Valentina", Edad = 24, FechaIngresoALaEmpresa = new DateTime(2021, 7, 8), Soltero = false },
+    new Persona { Nombre = "Roberto", Edad = 61, FechaIngresoALaEmpresa = DateTime.Now.AddDays(-1), Soltero = false },
+ };
+
+bool estaElItemNidia = personas.Contains(
+                                 new Persona
+                                 {
+                                     Nombre = "Nidia",
+                                     Edad = 19,
+                                     FechaIngresoALaEmpresa = new DateTime(2015, 11, 22),
+                                     Soltero = true
+                                 });
+
+Console.WriteLine($"estaElItemNidia: {estaElItemNidia}"); //false
+
+var nidia = personas[2];
+bool estaElItemNidia2 = personas.Contains((Persona)nidia);
+Console.WriteLine($"estaElItemNidia2: {estaElItemNidia2}"); //true
+
+
+
+bool estaElItemNidiaUsandoCustomComparer = personas.Contains(
+                                 new Persona
+                                 {
+                                     Nombre = "Nidia",
+                                     Edad = 19,
+                                     FechaIngresoALaEmpresa = new DateTime(2015, 11, 22),
+                                     Soltero = true
+                                 });
+Console.WriteLine($"estaElItemNidiaUsandoCustomComparer: {estaElItemNidiaUsandoCustomComparer}");
+
+
+/* Para poder comparar el contenido de un objeto hay que implementar IEqualityComparer */
+
+var puntos = new List<Punto>() {
+    new Punto { X = 10, Y = 20 },
+    new Punto { X = 11, Y = 21 },
+    new Punto { X = 12, Y = 22 },
+    new Punto { X = 13, Y = 23 },
+    new Punto { X = 14, Y = 24 },
+ };
+
+bool estaElItem1121UandoCustomComparer = puntos.Contains(
+    new Punto { X = 11, Y = 21 },
+    new PuntoComparer());
+
+Console.WriteLine($"estaElItem1121UandoCustomComparer: {estaElItem1121UandoCustomComparer}"); //true
+
---
+++ LINQ/CONTAINS/Punto.cs
@@ -0,0 +1,27 @@
+﻿using System.Diagnostics.CodeAnalysis;
+
+namespace CONTAINS
+{
+    public class Punto
+    {
+        public int X { get; set; }
+        public int Y { get; set; }
+        public override string ToString()
+        {
+            return $"({X},{Y})";
+        }
+    }
+
+    public class PuntoComparer : IEqualityComparer<Punto>
+    {
+        public bool Equals(Punto? punto1, Punto? punto2)
+        {
+            return punto1.X == punto2.X && punto1.Y == punto2.Y;
+        }
+
+        public int GetHashCode([DisallowNull] Punto obj)
+        {
+            return obj.X ^ obj.Y;
+        }
+    }
+}
