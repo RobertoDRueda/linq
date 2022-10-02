0d6cd34e765b56ea4ce79610c08fc9b669b86917
RobertoRueda <roberto.daniel.rueda@gmail.com>
2022-10-02 13:14:49 -0300

    Add Append and Concat

---
+++ LINQ/APPEND_PREPEND/Program.cs
@@ -0,0 +1,52 @@
+﻿
+
+using ANY;
+
+int[] numeros = { 1, 2, 3, 4, 5, 6 };
+
+var numerosConElNumero7 = numeros.Append(7); // 1, 2, 3, 4, 5, 6, 7
+
+Console.WriteLine("numerosConElNumero7");
+foreach (var num in numerosConElNumero7)
+{
+    Console.WriteLine(num);
+}
+
+var numerosConElNumero0 = numeros.Prepend(0); //  0, 1, 2, 3, 4, 5, 6 
+
+Console.WriteLine("numerosConElNumero0");
+foreach (var num in numerosConElNumero0)
+{
+    Console.WriteLine(num);
+}
+
+var personas = new List<Persona>() {
+    new Persona { Nombre = "Eduardo", Edad = 30, FechaIngresoALaEmpresa = new DateTime(2021, 1, 2), Soltero = true },
+    new Persona { Nombre = "Nidia", Edad = 19, FechaIngresoALaEmpresa = new DateTime(2015, 11, 22), Soltero = true },
+    new Persona { Nombre = "Alejandro", Edad = 45, FechaIngresoALaEmpresa = new DateTime(2020, 4, 12), Soltero = false },
+    new Persona { Nombre = "Valentina", Edad = 24, FechaIngresoALaEmpresa = new DateTime(2021, 7, 8), Soltero = false },
+    new Persona { Nombre = "Roberto", Edad = 60, FechaIngresoALaEmpresa = DateTime.Now.AddDays(-1), Soltero = false },
+    new Persona { Nombre = "Julio", Edad = 61, FechaIngresoALaEmpresa = DateTime.Now.AddDays(-1), Soltero = false },
+ };
+
+var personasConAndrea = personas.Append(
+                            new Persona { 
+                                Nombre = "Andrea", 
+                                Edad = 18, 
+                                FechaIngresoALaEmpresa = DateTime.Now.AddDays(-1), 
+                                Soltero = true 
+                            });
+Console.WriteLine("personasConAndrea");
+foreach (var persona in personasConAndrea)
+{
+    Console.WriteLine(persona);
+}
+
+var levels = new List<string> { "Malo", "Intermedio", "Bueno"};
+var newLevels = levels.Prepend("Terrible").Append("Excelente");
+
+Console.WriteLine("newLevels");
+foreach (var level in newLevels)
+{
+    Console.WriteLine(level);
+}
---
+++ LINQ/CONCAT/Program.cs
@@ -0,0 +1,60 @@
+﻿
+// Concat se usa para concatenar dos secuencias en una secuencia.
+// los argumentos no pueden ser nulos
+
+char[] sequence1 = { 'p', 'q', 'r', 's', 'y', 'z' };
+char[] sequence2 = { 'p', 'm', 'o', 'e', 'c', 'z' };
+
+
+Console.WriteLine("Sequence 1 is: ");
+
+foreach (var s1 in sequence1)
+{
+    Console.WriteLine(s1);
+}
+
+Console.WriteLine("Sequence 2 is: ");
+
+foreach (var s2 in sequence2)
+{
+    Console.WriteLine(s2);
+}
+
+var result = sequence1.Concat(sequence2);
+
+Console.WriteLine("New Sequence:");
+foreach (var val in result)
+{
+    Console.WriteLine(val);
+}
+
+
+List<int> numbers1 = new List<int> { 1, 2, 3, 4 };
+List<int> numbers2 = null;
+
+try
+{
+    var resultNum = numbers1.Concat(numbers2);
+    foreach (var item in resultNum)
+    {
+        Console.WriteLine(item);
+    }
+}
+catch (Exception e) 
+{ 
+    Console.WriteLine($"El valor del segundo argumento no puede ser nulo: {e} "); 
+}
+
+
+try
+{
+    var resultNum2 = numbers2.Concat(numbers1);
+    foreach (var item in resultNum2)
+    {
+        Console.WriteLine(item);
+    }
+}
+catch (Exception e)
+{
+    Console.WriteLine($"El valor del primer argumento no puede ser nulo: {e} ");
+}
